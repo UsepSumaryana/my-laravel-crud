3cae1f782eea64e9ea416627d5b276cc70fb77ab
neuron.usep <usep.neuronworks.co.id>
2023-05-26 20:12:09 +0700

    set landing page

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [\App\Http\Controllers\EmployeeController::class, 'index']);
 Route::resource('employees', \App\Http\Controllers\EmployeeController::class);
